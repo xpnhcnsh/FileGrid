f0a23d396381aa26122ef60ac66559846de747ae
xpnhcnsh <xpnhcnsh@gmail.com>
2025-06-14 00:02:33 +0800

    TBD: 继续优化ProjectEditTab.razor的布局

--- FileGrid/obj/Debug/net9.0/FileGrid.AssemblyInfo.cs
+++ FileGrid/obj/Debug/net9.0/FileGrid.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FileGrid")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+723d66fdc6d8d19ed55556957916a50670cee334")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44136642fe151cd42e36ca2a23dde744079162a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("FileGrid")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FileGrid")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
